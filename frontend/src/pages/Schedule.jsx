127ec742dc26fcd0f033a012c4d3ec19c35c962a
Alison Williams <alimarie@gmail.com>
2026-06-18 12:16:16 -0600

    Update Soul Motion event modal link to use full title

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -301,7 +301,7 @@ export default function Schedule() {
                 </td>
                 <td className="glance-event-cell"></td>
                 <td className="glance-event-cell">
-                  <div className="glance-event-block" onClick={() => openEventModal('Soul Motion')}>
+                  <div className="glance-event-block" onClick={() => openEventModal('Soul Motion: Opening the Gates')}>
                     <div className="glance-title">Soul Motion</div>
                     <div className="glance-facilitators">Devorah Bry</div>
                   </div>
